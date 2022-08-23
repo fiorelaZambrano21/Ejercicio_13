7a59f17b97a0ea5fd83f35cdbc616ba60767acc7
Fiorela Zambrano <fiorela.zambrano@unas.edu.pe>
2022-08-23 16:17:14 -0500

    texto en button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 <!DOCTYPE html>
 <h1>serie fibonacci </h1>
-<a class="btn btn-primary" href="/welcome" role="button">Generar numero Fibonacci</a>
+<a class="btn btn-primary" href="/welcome" role="button">MOSTRAR LA SERIE FIBONACCI MENOR A 100000</a>
 </html>
