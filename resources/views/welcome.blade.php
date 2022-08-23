87ef851a341480a362475e43438c5c80625199c4
Junior Saul Maiz Echevarria <junior.maiz@unas.edu.pe>
2022-08-23 10:39:40 -0500

    creacion de ruta y titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 <!DOCTYPE html>
-<h1>hola mundo </h1>
-<a class="btn btn-primary" href="#" role="button">Generar numero Fibonacci</a>
+<h1>serie fibonacci </h1>
+<a class="btn btn-primary" href="/welcome" role="button">Generar numero Fibonacci</a>
 </html>
