4dccaec9c83b960120f8be26f5361d1976896b89
Junior Saul Maiz Echevarria <junior.maiz@unas.edu.pe>
2022-08-23 19:21:23 -0500

    modificación

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 <!DOCTYPE html>
 <h1>serie fibonacci </h1>
-<a class="btn btn-primary" href="/welcome" role="button">Generar numero Fibonacci</a>
+<a class="btn btn-primary" href="/mostrar" role="button">Generar numero Fibonacci</a>
 </html>
